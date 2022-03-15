4e0a78e74a00e3ee3f0b9e52eff8cd46f19f8985
PunithGowda <punithmadaiahkumar@gmail.com>
2022-03-15 11:33:32 +0530

    Testing Article Model Part 2

--- articles/models.py
+++ articles/models.py
@@ -28,14 +28,14 @@ class Article(models.Model):
         # do another something
 
 def article_pre_save(sender,instance, *args, **kwargs):
-    print('pre_save')
+    # print('pre_save')
     if instance.slug is None:
         slugify_instance_title(instance) 
 
 pre_save.connect(article_pre_save, sender=Article)
 
 def article_post_save(sender, instance, created,*args, **kwargs):
-    print('post_save')
+    # print('post_save')
     if created:
         slugify_instance_title(instance, save=True) 
 
