7913e93a37b6453266908c368a1ec566839babf4
PunithGowda <punithmadaiahkumar@gmail.com>
2022-03-15 15:29:28 +0530

    Complex Search using Django Q Lookups

--- articles/views.py
+++ articles/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.decorators import login_required
+from django.db.models import Q
 from django.http import Http404
 from django.shortcuts import render, redirect
 
@@ -6,19 +7,13 @@ from .forms import ArticleForm
 from .models import Article
 # Create your views here.
 def article_serach_view(request):
-    #print(request.GET)
-    query_dict = request.GET #this is a dictionary
-    # query = query_dict.get("q") #<input type='text' name='q'/>
-    
-    try:
-        query = int(query_dict.get("q"))
-    except:
-        query = None
-    article_obj = None
+    query = request.GET.get('q')
+    qs = Article.objects.all()
     if query is not None:
-        article_obj = Article.objects.get(id=query)
+        lookups = Q(title__icontains=query) | Q(content__icontains=query)
+        qs = Article.objects.filter(lookups)
     context = {
-        "object": article_obj
+        "object_list": qs
     }
     return render(request, "articles/search.html", 
     context=context)
