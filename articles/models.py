518235c900371cbb7f02cb38bb8371a715c3e658
PunithGowda <punithmadaiahkumar@gmail.com>
2022-03-15 15:57:55 +0530

    Model Managers & Custom QuerySets for Search

--- articles/models.py
+++ articles/models.py
@@ -1,5 +1,6 @@
 
 from django.db import models
+from django.db.models import Q
 from django.db.models.signals import pre_save, post_save
 from django.urls import reverse
 from django.utils import timezone
@@ -7,6 +8,21 @@ from django.utils import timezone
 from .utils import slugify_instance_title
  
 # Create your models here.  
+class ArticleQuerySet(models.QuerySet):
+    def search(self, query=None):
+        if query is None or query == "":
+            return self.none()
+        lookups = Q(title__icontains=query) | Q(content__icontains=query)
+        return self.filter(lookups) 
+
+class ArticleManager(models.Manager):
+    def get_queryset(self):
+        return ArticleQuerySet(self.model, using=self._db)
+
+    def search(self, query=None):
+        return self.get_queryset().search(query=query)
+
+
 class Article(models.Model):
     title = models.CharField(max_length=120)
     slug = models.SlugField(unique=True,blank=True, null=True)
@@ -14,6 +30,8 @@ class Article(models.Model):
     timestamp = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     publish = models.DateField(auto_now=False, auto_now_add=False, null=True, blank=True)
+
+    objects = ArticleManager()
 
     def get_absolute_url(self):
         # return f'/articles/{self.slug}/'
--- articles/views.py
+++ articles/views.py
@@ -1,5 +1,4 @@
 from django.contrib.auth.decorators import login_required
-from django.db.models import Q
 from django.http import Http404
 from django.shortcuts import render, redirect
 
@@ -8,10 +7,7 @@ from .models import Article
 # Create your views here.
 def article_serach_view(request):
     query = request.GET.get('q')
-    qs = Article.objects.all()
-    if query is not None:
-        lookups = Q(title__icontains=query) | Q(content__icontains=query)
-        qs = Article.objects.filter(lookups)
+    qs = Article.objects.search(query=query)
     context = {
         "object_list": qs
     }
