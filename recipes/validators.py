5a8242ea879ea764d8d662bd6c7967e0c361c2de
PunithGowda <punithmadaiahkumar@gmail.com>
2022-03-17 12:49:02 +0530

    updated

--- recipes/validators.py
+++ recipes/validators.py
@@ -1,11 +1,15 @@
 import pint
 from django.core.exceptions import ValidationError
+from pint.errors import UndefinedUnitError
 
 
 valid_unit_measurments = ['pounds', 'lbs', 'oz', 'gram']
 
 def validate_unit_of_measure(value):
     ureg = pint.UnitRegistry() 
-    single_unit = ureg[value]
-    # if value not in valid_unit_measurments:
-    #     raise ValidationError(f"{value} is not a valid unit of measure")
+    try:
+        single_unit = ureg[value.lower()]
+    except UndefinedUnitError as e:
+        raise ValidationError(f"'{value}' is not a valid unit of measure")
+    except:
+        raise ValidationError(f"'{value}' is invalid. Unknown error.")
