cf38c99b6bb92233eeb9b7ae087df8e32a952ab6
PunithGowda <punithmadaiahkumar@gmail.com>
2022-03-21 13:05:18 +0530

    created Views for recipe

--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def recipe_list_view(request, id=None):
+    return 
+
+def recipe_detail_view(request, id=None):
+    return 
+
+def recipe_create_view(request, id=None):
+    form = RecipeForm(request.POST or None)
+    return
+
+def recipe_update_view(request, id=None):
+    form = RecipeForm(request.POST or None)
+    return 
