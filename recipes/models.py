6f33030a4b4b113519cd5710f1fbd57ec83df197
PunithGowda <punithmadaiahkumar@gmail.com>
2022-03-22 11:24:57 +0530

    CRUD Views for Recipe Model

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,4 @@
+from django.shortcuts import redirect
 import pint
 from django.conf import settings
 from django.db import models
@@ -25,6 +26,9 @@ class Recipe(models.Model):
     updated = models.DateTimeField(auto_now=True)
     active = models.BooleanField(default=True)
 
+    def get_absolute_url(self):
+        return "/pantry/recipes"
+
 class RecipeIngredient(models.Model):
     recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
     name = models.CharField(max_length=220)
@@ -37,6 +41,9 @@ class RecipeIngredient(models.Model):
     timestamp = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     active = models.BooleanField(default=True)
+
+    def get_absolute_url(self):
+        return self.recipe.get_absolute_url()
 
     def convert_to_system(self, system="mks"):
         if self.quantity_as_float is None:
--- recipes/views.py
+++ recipes/views.py
@@ -1,18 +1,46 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect, render, get_object_or_404
+
+from .forms import RecipeForm
+from .models import Recipe
 
 # Create your views here.
 
+@login_required
+def recipe_list_view(request):
+    qs = Recipe.objects.filter(user=request.user)
+    context = {
+        "object_list": qs
+    }
+    return render(request, "recipes/list.html", context)
 
-def recipe_list_view(request, id=None):
-    return 
 
+@login_required
 def recipe_detail_view(request, id=None):
-    return 
+    obj = get_object_or_404(Recipe, id=id, user=request.user)
+    context = {
+        "object": obj
+    }
+    return render(request, "recipes/detail.html", context)
 
-def recipe_create_view(request, id=None):
+
+@login_required
+def recipe_create_view(request):
     form = RecipeForm(request.POST or None)
+    if form.is_valid():
+        obj = form.save(commit=False)
+        obj.user = request.user
+        obj.save()
+        return redirect(obj.get_absolute_url())
     return
 
+
+@login_required
 def recipe_update_view(request, id=None):
     form = RecipeForm(request.POST or None)
-    return 
+    obj = get_object_or_404(Recipe, id=id, user=request.user)
+    context = {
+        "form": form,
+        "object": obj
+    }
+    return render(request, "recipes/detail.html", context)
