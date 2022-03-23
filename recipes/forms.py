ea249c49a3cf8c2e4d9b38e25b621b2c66dff49c
PunithGowda <punithmadaiahkumar@gmail.com>
2022-03-23 10:25:49 +0530

    Django 2 Forms, 1 View

--- recipes/forms.py
+++ recipes/forms.py
@@ -1,8 +1,13 @@
 from django import forms
 
-from .models import Recipe
+from .models import Recipe, RecipeIngredient
 
 class RecipeForm(forms.ModelForm):
     class Meta:
         model = Recipe
         fields = ['name', 'description', 'directions']
+
+class RecipeIngredientForm(forms.ModelForm):
+    class Meta:
+        model = RecipeIngredient
+        fields = ['name', 'quantity', 'unit']
