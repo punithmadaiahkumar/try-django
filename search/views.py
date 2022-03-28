cd612c6fef150f8b0bc740574a277b3b5ee235e7
PunithGowda <punithmadaiahkumar@gmail.com>
2022-03-28 11:40:53 +0530

    HTMX Typeahead & Search in Django

--- search/views.py
+++ search/views.py
@@ -1,13 +1,29 @@
 from django.shortcuts import render
 
-# Create your views here.
+from articles.models import Article
+from recipes.models import Recipe
+
+SEARCH_TYPE_MAPPING ={
+    'articles': Article,
+    'article': Article,
+    'recipes': Recipe,
+    'recipe': Recipe,
+}
+
+
 def search_view(request):
     query = request.GET.get('q')
-
+    search_type = request.GET.get('type')
+    Klass = Recipe
+    if search_type in SEARCH_TYPE_MAPPING.keys():
+        Klass = SEARCH_TYPE_MAPPING[search_type]
+    qs = Klass.objects.search(query=query)
     context = {
-        "query":query
+        "queryset": qs
     }
     template = "search/results-view.html"
     if request.htmx:
+        context['queryset'] = qs[:5]
         template = "search/partials/results.html"
     return render(request, template, context)
+
