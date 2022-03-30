e3477837cf50b43eb7b3f91b7b5e3593401762fe
PunithGowda <punithmadaiahkumar@gmail.com>
2022-03-30 18:14:36 +0530

    Uploading Files with Django & HTMX

--- recipes/views.py
+++ recipes/views.py
@@ -1,5 +1,6 @@
 from audioop import reverse
 from email import header
+from re import template
 from typing_extensions import Self
 from wsgiref import headers
 from django.contrib.auth.decorators import login_required
@@ -172,6 +173,9 @@ def recipe_ingredient_update_hx_view(request, parent_id=None, id=None):
 
 
 def recipe_ingredient_image_upload_view(request, parent_id=None):
+    template_name = "recipes/upload-image.html"
+    if request.htmx:
+        template_name = "recipes/partials/image-upload-form.html"
     try:
         parent_obj = Recipe.objects.get(id=parent_id, user=request.user)
     except:
@@ -184,4 +188,4 @@ def recipe_ingredient_image_upload_view(request, parent_id=None):
         obj.recipe = parent_id
         # obj.recipe_id = parent_id
         obj.save()
-    return render(request, "image-form.html", {"form":form})
+    return render(request, template_name, {"form":form})
